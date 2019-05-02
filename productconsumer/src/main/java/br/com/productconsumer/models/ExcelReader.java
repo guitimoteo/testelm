3316a1cb71bc529514a2f199200b7fcb59a85286
guitimoteo <guilherme.timoteo@dellavolpe.com.br>
2019-05-02 20:10:26 -0300

    adicionando lista de produtos a serem gravados.

--- productconsumer/src/main/java/br/com/productconsumer/models/ExcelReader.java
+++ productconsumer/src/main/java/br/com/productconsumer/models/ExcelReader.java
@@ -2,6 +2,7 @@ package br.com.productconsumer.models;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.poi.EncryptedDocumentException;
@@ -26,7 +27,8 @@ public class ExcelReader {
 	public List<Product> listProducts() {
 		Sheet sheet = workbook.getSheetAt(0);
 		Double category = sheet.getRow(0).getCell(1).getNumericCellValue();
-		sheet.forEach(r-> new Product(category, r.getCell(0).getStringCellValue(), r.getCell(1).getStringCellValue(), r.getCell(2).getBooleanCellValue(), r.getCell(3).getStringCellValue(), r.getCell(4).getNumericCellValue()));
-		return null;
+		List<Product> products = new ArrayList<>();
+		sheet.forEach(r-> products.add(new Product(category, r.getCell(0).getStringCellValue(), r.getCell(1).getStringCellValue(), r.getCell(2).getBooleanCellValue(), r.getCell(3).getStringCellValue(), r.getCell(4).getNumericCellValue())));
+		return products;
 	}
 }
