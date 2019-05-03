66b711a70712b8980ad2ab649225f85308c5ba94
guitimoteo <guilherme.timoteo@dellavolpe.com.br>
2019-05-03 18:16:38 -0300

    transferindo codigo para a service

--- productconsumer/src/main/java/br/com/productconsumer/messages/Receiver.java
+++ productconsumer/src/main/java/br/com/productconsumer/messages/Receiver.java
@@ -1,26 +1,28 @@
 package br.com.productconsumer.messages;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import br.com.productconsumer.dto.FileMessageDto;
 import br.com.productconsumer.models.ExcelReader;
-import br.com.productconsumer.models.Product;
+import br.com.productconsumer.services.ProductService;
 
 @Component
 public class Receiver {
 
 	private static final Logger logger = LoggerFactory.getLogger(Receiver.class);
-
+	
+	@Autowired
+	private ProductService productService;
+	
 	@KafkaListener(topics = "${application.topic}")
 	public void receive(FileMessageDto fileDto) {
 		logger.info("receive: {}", fileDto.getName());
 		try {
-			List<Product> products = new ExcelReader(fileDto).listProducts();
+			productService.createProducts(new ExcelReader(fileDto).listProducts());
 		} catch (RuntimeException e) {
 			logger.error("Erro ao converter o arquivo: {}, {}", e.getMessage(), e.getCause());
 			e.printStackTrace();
