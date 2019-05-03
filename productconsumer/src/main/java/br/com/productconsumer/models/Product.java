c4177a093abd96421dd3926f64a177ce2ecea440
guitimoteo <guilherme.timoteo@dellavolpe.com.br>
2019-05-03 18:15:40 -0300

    adicionando propriedades do mongodb

--- productconsumer/src/main/java/br/com/productconsumer/models/Product.java
+++ productconsumer/src/main/java/br/com/productconsumer/models/Product.java
@@ -1,14 +1,29 @@
 package br.com.productconsumer.models;
 
 import org.apache.poi.ss.usermodel.Row;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+import com.mongodb.lang.NonNull;
+
+@Document("products")
 public class Product {
-	
-	private Double category;
+	@Id
+	@NonNull
+	@Field("lm")
 	private Double lm;
+	@NonNull
+	@Field("category")
+	private Double category;
+	@NonNull
+	@Field("name")
 	private String name;
-	private Boolean free_shipping;	
+	@Field("free_shipping")
+	private Boolean freeShipping;	
+	@Field("description")
 	private String description;	
+	@Field("price")
 	private Double price;
 	
 	public Product(Double category, Row row) {
@@ -16,7 +31,7 @@ public class Product {
 		this.category = category;
 		this.lm = row.getCell(0).getNumericCellValue();
 		this.name = row.getCell(1).getStringCellValue();
-		this.free_shipping = row.getCell(2).getNumericCellValue() == 1;
+		this.freeShipping = row.getCell(2).getNumericCellValue() == 1;
 		this.description = row.getCell(3).getStringCellValue();
 		this.price = Double.valueOf(row.getCell(4).getStringCellValue());
 	}
