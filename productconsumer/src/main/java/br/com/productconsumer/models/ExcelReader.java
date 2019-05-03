0875fefdfbcd7488194d3e05655dff5a8971b6bf
guitimoteo <guilherme.timoteo@dellavolpe.com.br>
2019-05-03 18:14:15 -0300

    melhorando o o codigo

--- productconsumer/src/main/java/br/com/productconsumer/models/ExcelReader.java
+++ productconsumer/src/main/java/br/com/productconsumer/models/ExcelReader.java
@@ -15,17 +15,19 @@ import br.com.productconsumer.dto.FileMessageDto;
 public class ExcelReader {
 
 	private Workbook workbook;
+	private Sheet sheet;
 
 	public ExcelReader(FileMessageDto fileDto) {
 		try {
 			this.workbook = HSSFWorkbookFactory.create(new ByteArrayInputStream(fileDto.getContent()));
+			sheet = workbook.getSheetAt(0);
+			this.workbook.close();
 		} catch (EncryptedDocumentException | IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 	
 	public List<Product> listProducts() {
-		Sheet sheet = workbook.getSheetAt(0);
 		Double category = sheet.getRow(0).getCell(1).getNumericCellValue();
 		List<Product> products = new ArrayList<>();
 		sheet.forEach(r -> {if(r.getRowNum() > 2) products.add(new Product(category, r));});
