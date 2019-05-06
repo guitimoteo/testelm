4907b6e0311369d3d3be5a760a94766bf19200dc
guitimoteo <guitimoteo@yahoo.com.br>
2019-05-06 00:14:41 -0300

    retirando redundancia

--- productconsumer/src/main/java/br/com/productconsumer/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.productconsumer.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.product.commons.models.Product;
-
-public interface ProductRepository extends CrudRepository<Product, Double> {
-}
