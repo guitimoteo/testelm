fe38498b48141f8a69cc368a1962a333ee9bef5b
guitimoteo <guitimoteo@yahoo.com.br>
2019-05-08 21:34:49 -0300

    limpando imports da aplicacao.

--- productserver/src/test/java/br/com/productserver/services/ProductServiceUnitTest.java
+++ productserver/src/test/java/br/com/productserver/services/ProductServiceUnitTest.java
@@ -2,27 +2,18 @@ package br.com.productserver.services;
 
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
-
-import org.apache.poi.EncryptedDocumentException;
 import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.multipart.MultipartFile;
 
 import br.com.product.commons.dtos.FileMessageDto;
 import br.com.product.commons.dtos.ProductDto;
